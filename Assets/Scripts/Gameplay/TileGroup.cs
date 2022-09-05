d1625d814f1a16a0ee3e934265355726c9f10729
Harryanto <harry@hwinsen.com>
2022-09-05 15:54:36 +0800

    Add random spawn to tile and make tile can change sprite

--- Assets/Scripts/Gameplay/TileGroup.cs
+++ Assets/Scripts/Gameplay/TileGroup.cs
@@ -1,3 +1,4 @@
+using Agate.MVC.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,46 +8,95 @@ namespace MatchPicture.Tile
 {
     public class TileGroup : MonoBehaviour
     {
-        [SerializeField] private int column;
-        [SerializeField] private int row;
-        [SerializeField] private GameObject tile;
-        [SerializeField] private int themeID;
-        [SerializeField] private SpriteAtlas tileAtlas;
-        //[SerializeField] private Dictionary<Vector2, GameObject> _tiles = new Dictionary<Vector2, GameObject>();
+        [SerializeField] private int _column;
+        [SerializeField] private int _row;
+        [SerializeField] private GameObject _tile;
+        [SerializeField] private int _themeID;
+        public int[] spriteArray;
+        public SpriteAtlas tileAtlas;
+        private Dictionary<Vector2, GameObject> _tiles = new Dictionary<Vector2, GameObject>();
+        [SerializeField] private List<GameObject> _tileList = new();
 
         // Start is called before the first frame update
         void Start()
         {
-            themeID = 0;
-            SpawnTile(themeID);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        void SpawnTile(int id)
-        {
-            for (int x = 0; x < row; x++)
+            if ( _row % 2 == 1)
             {
-                for (int y = 0; y < column; y++)
+                _row++;
+            }
+            if (_column % 2 == 1)
+            {
+                _column++;
+            }
+
+            SetSpriteArray();
+            SpawnTile();
+
+        }
+
+        void SpawnTile()
+        {
+            for (int x = 0; x < _row; x++)
+            {
+                for (int y = 0; y < _column; y++)
                 {
-                    var spawnedTile = Instantiate(tile, new Vector2(x, y), Quaternion.identity);
+                    var spawnedTile = Instantiate(_tile, new Vector2(x, y), Quaternion.identity);
                     spawnedTile.name = $"Tile {x} {y}";
                     spawnedTile.AddComponent<TileObject>();
-                    //_tiles[new Vector2(x, y)] = spawnedTile;
-                    //spawnedTile.transform.parent = gameObject.transform;
+                    _tiles[new Vector2(x, y)] = spawnedTile;
+                    _tileList.Add(spawnedTile);
+                    spawnedTile.transform.parent = gameObject.transform;
                 }
             }
 
             // centering
-            //foreach (GameObject _o in _tiles.Values)
-            //{
-            //    _o.transform.position -= new Vector3(_width / 2, _height / 4);
-            //    //Debug.Log(_o.transform.position);
-            //}
+            foreach (GameObject _o in _tiles.Values)
+            {
+                _o.transform.position -= new Vector3(_row / 2, _column / 4);
+                //_o.GetComponent<SpriteRenderer>().sprite
+                //Debug.Log(_o.transform.position);
+            }
+        }
+
+        void SetTileTheme()
+        {
+            // theme di load dari save data
+        }
+
+        void SetSpriteArray()
+        {
+            spriteArray = new int[(_row * _column)];
+
+            // get first half of spriteArray
+            for (int arrayFirstHalf = 0; arrayFirstHalf < (_row * _column) / 2; arrayFirstHalf++)
+            {
+                int randomIndex = Random.Range(0, tileAtlas.spriteCount);
+                spriteArray[arrayFirstHalf] = randomIndex;
+            }
+
+            // clone first half to second half of spriteArray
+            for (int arraySecondHalf = (_row * _column) / 2; arraySecondHalf < (_row * _column); arraySecondHalf++)
+            {
+                spriteArray[arraySecondHalf] = spriteArray[arraySecondHalf - (_row * _column) / 2];
+            }
+
+            // shuffle spriteArray
+            for (int i = 0; i < spriteArray.Length; i++)
+            {
+                int rnd = Random.Range(0, spriteArray.Length);
+                int tempGO = spriteArray[rnd];
+                spriteArray[rnd] = spriteArray[i];
+                spriteArray[i] = tempGO;
+            }
+        }
+
+        public Sprite ChangeSprite(GameObject gameObject)
+        {
+            var myKey = _tileList.IndexOf(gameObject);
+            Debug.Log(myKey);
+
+            string tileAtlasName = tileAtlas.name;
+            return tileAtlas.GetSprite(tileAtlasName + "_" + spriteArray[myKey]);
         }
     }
 }
--- Assets/Scripts/Gameplay/TileObject.cs
+++ Assets/Scripts/Gameplay/TileObject.cs
@@ -8,28 +8,26 @@ namespace MatchPicture.Tile
     public class TileObject : MonoBehaviour, IRaycastable
     {
         [SerializeField] private bool _isRaycasted;
+        [SerializeField] private Sprite _defaultSprite;
 
         // Start is called before the first frame update
         void Start()
         {
             _isRaycasted = false;
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            _defaultSprite = GetComponent<SpriteRenderer>().sprite;
         }
 
         public void OnRaycasted()
         {
             if (_isRaycasted)
             {
+                GetComponent<SpriteRenderer>().sprite = _defaultSprite;
                 _isRaycasted = false;
                 Debug.Log(gameObject.name + " raycasted: " + _isRaycasted);
             }
             else
             {
+                GetComponent<SpriteRenderer>().sprite = transform.parent.GetComponent<TileGroup>().ChangeSprite(gameObject);
                 _isRaycasted = true;
                 Debug.Log(gameObject.name + " raycasted: " + _isRaycasted);
             }
